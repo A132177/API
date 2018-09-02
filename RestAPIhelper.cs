2e27383ef7d5caeabb3f58fc284738a2d29a80ad
A132177 <mdutta@agl.com.au>
2018-09-02 21:56:39 +0530

    ResAPI helper class
    Methods for the test class

---
+++ RestAPIhelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using RestSharp;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace API_automation_Test_Suit
+{
+    public static class RestAPIhelper
+    {
+        public static RestClient client;
+        public static RestRequest restRequest;
+        public static string baseUrl = "http://mydomain.com";
+
+        public static RestClient SetUrl(string endpoint)
+        {
+            var Url = Path.Combine(baseUrl, endpoint);
+            return client = new RestClient(Url);
+        }
+
+        public static RestRequest CreateRequest()
+        {
+            restRequest = new RestRequest(Method.GET);
+            restRequest.AddHeader("Accept", "application/Json");
+            return restRequest;
+        }
+
+        public static IRestResponse GetResponse()
+        {
+            return client.Execute(restRequest);
+        }
+      
+    }
+}
